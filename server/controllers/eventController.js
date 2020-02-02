1b9128ecff6841fe048844243bdcfd22a05afb07
dodeca-dev <jrussclay@gmail.com>
2020-02-02 21:48:26 +0800

    add crud operations for event

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -2,11 +2,23 @@ const Event = require("../models/Event");
 
 exports.getEvents = async (req, res) => {
   const events = await Event.find();
-  res.status(200).json({ success: true, events });
+  res.status(200).json({ success: true, data: events, count: events.length });
 };
 
-exports.getEvent = (req, res) => {
-  res.status(200).json({ success: true, message: "Get single event" });
+exports.getEvent = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+    if (!event)
+      return res
+        .status(400)
+        .json({ success: false, message: "Cannot find this event" });
+    res.status(200).json({ success: true, data: event });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "Cannot fetch this event" });
+  }
 };
 
 exports.createEvent = async (req, res) => {
@@ -19,10 +31,35 @@ exports.createEvent = async (req, res) => {
   }
 };
 
-exports.deleteEVent = (req, res) => {
-  res.status(200).json({ success: true, message: "Delete an event" });
+exports.deleteEVent = async (req, res) => {
+  try {
+    const event = await Event.findByIdAndDelete(req.params.id);
+    if (!event)
+      return res
+        .status(400)
+        .json({ success: false, message: "Cannot find event" });
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Cannot delete an event" });
+  }
 };
 
-exports.updateEvent = (req, res) => {
-  res.status(200).json({ success: true, message: "Update an event" });
+exports.updateEvent = async (req, res) => {
+  try {
+    const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!event)
+      return res
+        .status(400)
+        .json({ success: false, message: "Cannot find event" });
+    res.status(200).json({ success: true, data: event });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error in updating event" });
+  }
 };
--- server/models/Event.js
+++ server/models/Event.js
@@ -4,8 +4,11 @@ const EventSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      require: [true, "Name is required"]
+      require: [true, "Name is required"],
+      unique: true,
+      trim: true
     },
+    slug: String,
     description: {
       type: String,
       require: [true, "Description is required"],
@@ -14,6 +17,44 @@ const EventSchema = mongoose.Schema(
     address: {
       type: String,
       required: [true, "Address is required"]
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"]
+      },
+      coordinates: {
+        type: [Number],
+        index: "2dsphere"
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      country: String
+    },
+    website: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        "Please use a valid URL with HTTP or HTTPS"
+      ]
+    },
+    phone: {
+      type: String,
+      maxlength: [20, "Phone number can not be longer than 20 characters"]
+    },
+    email: {
+      type: String,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email"
+      ]
+    },
+    imageURL: {
+      type: String,
+      default: "no-photo.jpg"
     },
     attendees: [
       {
